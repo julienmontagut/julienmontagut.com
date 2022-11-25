6b477a9827577e285c2549041ee7d20e14270a12
Julien Montagut <_@julienmontagut.com>
2022-11-25 23:19:16 +0000

    Update styles with tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			"space-cadet": "#1e1e34",
+  			"lavender-web": "#e4e4f1",
+			"ultramarine": "#4a0ffa",
+			"rusty-red": "#da2c43"
+		}
 	},
 	plugins: [],
 }
