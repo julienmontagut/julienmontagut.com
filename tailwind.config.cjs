838c2ba0fa78d28c84c7f68dc49b0e146d5abd0e
Julien Montagut <_@julienmontagut.com>
2022-11-29 00:03:30 +0100

    Update website home and about pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
-			"space-cadet": "#1e1e34",
-  			"lavender-web": "#e4e4f1",
-			"ultramarine": "#4a0ffa",
-			"rusty-red": "#da2c43"
-		}
+		extend: {
+			colors: {
+				"space": "#1e1e34",
+				"rust": "#da2c43",
+				"ultramarine": "#4a0ffa",
+			},
+		},
 	},
 	plugins: [],
 }
